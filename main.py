1b4a475fed27875db86111b0752a5922d17ead06
lotlouischoitslab <louis.s.cho@gmail.com>
2023-06-20 18:47:34 -0500

    add

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.utils import Vehicle
 
+import matplotlib.pyplot as plt
+
 def main():
     # Instantiate a Vehicle
     vehicle = Vehicle(v_max=33.3, a=0.3, b=3, T=1.5, s_0=2, delta=4)
@@ -11,10 +13,35 @@ def main():
     v_front = 20
     s_front = 100
 
+    # Lists to store speed and position over time
+    time = []
+    speed = []
+    position = []
+
     # Simulation for 100 time steps
-    for t in range(1,100):
+    for t in range(100):
         v, s = vehicle.update(v_front, s_front, dt)
-        print(f"At time {t*dt}: speed = {v}, position = {s}")
+        time.append(t*dt)
+        speed.append(v)
+        position.append(s)
+
+    # Plot speed and position over time
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Speed (m/s)', color=color)
+    ax1.plot(time, speed, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('Position (m)', color=color)
+    ax2.plot(time, position, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.show()
 
 if __name__ =='__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ class Vehicle:
 
     def update(self, v_front, s_front, dt):
         s_star = self.s_0 + max(0, self.v*self.T + self.v*(self.v - v_front)/(2*np.sqrt(self.a*self.b)))
-        epsilon = 1e-10  # small constant to prevent division by zero
+        epsilon = 1e-10
         self.v += dt*(self.a*(1 - (self.v/self.v_max)**self.delta - (s_star/(self.s + epsilon))**2))
         self.s += self.v*dt
         return self.v, self.s
